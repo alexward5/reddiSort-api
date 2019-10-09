bba13e95c89f101585066d5f2fae899114de029c
Alexander Ward <alexanderward5@gmail.com>
2019-10-09 13:44:10 -0600

    reduced permissions required by app

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const reddit = simpleOAuth2Reddit.create({
   callbackURL: process.env.CALLBACK_URL,
   state: process.env.STATE,
   duration: 'permanent',
-  scope: ['*']
+  scope: ['history', 'identity']
 });
 
 const getData = async (access_token) => {
